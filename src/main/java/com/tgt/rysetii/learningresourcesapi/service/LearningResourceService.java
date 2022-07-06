d993e2545cf3880b42821da7b60ae18d7f973db7
dharshini0512 <dharshiniprasath05@gmail.com>
2022-07-06 16:39:54 +0530

    Method added to save data to csv and also sorted by profit margin

--- src/main/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceService.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/service/LearningResourceService.java
@@ -9,7 +9,13 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class LearningResourceService {
+
+    public void  saveLearningResources(List<LearningResource> learningResources){
+        populateLearningResourcesToCsv(learningResources);
+    }
     public List<LearningResource> getLearningResources(){
         File learningResourcesFile = new File("LearningResources.csv");
         List<LearningResource> learningResources = loadLearningResourceFromCsv(learningResourcesFile);
@@ -49,4 +55,55 @@ public class LearningResourceService {
         LearningResource learningResource = new LearningResource(learningResourceId, learningResourceName, costPrice, sellingPrice, learningResourceStatus, createdDate, publishedDate, retiredDate);
         return learningResource;
     }
+
+    private void populateLearningResourcesToCsv(List<LearningResource> learningResources){
+        final String csvDelimiter = ",";
+
+        try{
+            File learningResourcesFile = new File("LearningResources.csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(learningResourcesFile.getName(), true));
+            for (LearningResource learningResource : learningResources) {
+                bufferedWriter.newLine();
+                StringBuffer singleLine = new StringBuffer();
+                singleLine.append(learningResource.getLearningResourceId());
+                singleLine.append(csvDelimiter);
+                singleLine.append(learningResource.getLearningResourceName());
+                singleLine.append(csvDelimiter);
+                singleLine.append(learningResource.getCostPrice());
+                singleLine.append(csvDelimiter);
+                singleLine.append(learningResource.getSellingPrice());
+                singleLine.append(csvDelimiter);
+                singleLine.append(learningResource.getLearningResourceStatus());
+                singleLine.append(csvDelimiter);
+                singleLine.append(learningResource.getCreatedDate());
+                singleLine.append(csvDelimiter);
+                singleLine.append(learningResource.getPublishedDate());
+                singleLine.append(csvDelimiter);
+                singleLine.append(learningResource.getRetiredDate());
+                bufferedWriter.write(singleLine.toString());
+            }
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Double> getProfitMargin(){
+        List<LearningResource> learningResources = getLearningResources();
+        List<Double> profitMargin = learningResources.stream().map(lr -> ((lr.getSellingPrice()-lr.getCostPrice())/lr.getSellingPrice())).collect(toList());
+        return profitMargin;
+    }
+
+    public List<LearningResource> sortLearningResourcesByProfitMargin(){
+        List<LearningResource> learningResources = getLearningResources();
+        learningResources.sort((lr1,lr2) -> {
+            Double profitMargin1 = (lr1.getSellingPrice() - lr1.getCostPrice())/ lr1.getSellingPrice();
+            Double profitMargin2 = (lr2.getSellingPrice() - lr2.getCostPrice())/ lr2.getSellingPrice();
+            return profitMargin2.compareTo(profitMargin1);
+        });
+        return learningResources;
+    }
+
 }

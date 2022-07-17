aa845c1cb2169607f1d07e78c492e4fffc7bebe0
dharshini0512 <dharshiniprasath05@gmail.com>
2022-07-17 11:53:57 +0530

    GET, POST and DELETE endpoints created

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/controllers/LearningResourceController.java
@@ -0,0 +1,36 @@
+package com.tgt.rysetii.learningresourcesapi.controllers;
+
+
+import com.tgt.rysetii.learningresourcesapi.entity.LearningResource;
+import com.tgt.rysetii.learningresourcesapi.service.LearningResourceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/learningresources")
+public class LearningResourceController {
+
+    private final LearningResourceService learningResourceService;
+
+    public LearningResourceController(LearningResourceService learningResourceService) {
+        this.learningResourceService = learningResourceService;
+    }
+
+    @GetMapping("/")
+    public List<LearningResource> getAllLearningResources() {
+        return learningResourceService.getLearningResources();
+    }
+
+    @PostMapping(value = "/", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveLearningResources(@RequestBody List<LearningResource> learningResources) {
+        learningResourceService.saveLearningResources(learningResources);
+    }
+
+    @DeleteMapping(value = "/learningresource/{learningResourceId}")
+    public void deleteLearningResource(@PathVariable int learningResourceId) {
+        learningResourceService.deleteLearningResourceById(learningResourceId);
+    }
+}
